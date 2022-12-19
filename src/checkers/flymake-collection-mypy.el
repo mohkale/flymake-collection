74e2f3fb4b9b4944f1c90377d7d2f16e57d6d5cd
dmitrig <34632025+dmitrig@users.noreply.github.com>
2022-12-19 10:54:33 -0800

    Set the working dir and shadow file flag for mypy (#9)
    * Run mypy from inferred python project root
    
    * Remove leading dot from temp file name
    
    This appears to break relative imports in files checked by mypy. Maybe
    make the temp file pattern configurable per checker?
    
    * Use mypy --shadow-file for buffers with a file
    
    Fall back to the buffer name when `buffer-file-name' is nil. Necessary
    for checking temp files since python module names are based on the
    file name.
    
    * Parse mypy diagnostic ids
    
    * Make the mypy working dir configurable
    
    * Add basic tests for mypy
    
    * Add require for project
    
    * Move required mypy flags out of custom variable
    
    * Fix parsing of mypy error ids with hyphens
    
    * Add temp-file-prefix paramter
    
    * Update src/checkers/flymake-collection-mypy.el
    
    Co-authored-by: dmitrig <34632025+dgarbuzov@users.noreply.github.com>
    Co-authored-by: Mohsin Kaleem <mohkale@kisara.moe>

--- src/checkers/flymake-collection-mypy.el
+++ src/checkers/flymake-collection-mypy.el
@@ -26,11 +26,60 @@
 
 ;;; Code:
 
+(require 'project)
 (require 'flymake)
 (require 'flymake-collection)
 
 (eval-when-compile
   (require 'flymake-collection-define))
+
+(defcustom flymake-collection-mypy-args
+  '("--show-error-codes")
+  "Command line arguments always passed to `flymake-collection-mypy'."
+  :type '(repeat (string :tag "Arg"))
+  :group 'flymake-collection)
+
+(defcustom flymake-collection-mypy-project-root
+  '(("mypy.ini" "project.toml" "setup.cfg") project-root default-directory)
+  "Method to set project root."
+  :type '(repeat :tag "Run mypy from the first choice that succeeds"
+                 (choice (const :tag "The buffer default-directory" default-directory)
+                         (const :tag "The current project root" project-root)
+                         (directory :tag "A specific directory")
+                         (repeat :tag "The first ancestor directory containing"
+                                 :value ("mypy.ini" "pyproject.toml" "setup.cfg")
+                                 (string :tag "File name"))))
+  :group 'flymake-collection
+  :safe 'listp)
+
+(defun flymake-collection-mypy--locate-dominating-files (buffer files)
+  "Find ancestor directory of `BUFFER' containing any of `FILES'."
+  (let* ((start (if-let ((file (buffer-file-name buffer)))
+                    (file-name-directory file)
+                  (buffer-local-value 'default-directory buffer)))
+         (regex (mapconcat 'regexp-quote files "\\|"))
+         (root (locate-dominating-file
+                start (lambda (dir) (directory-files dir nil regex t)))))
+    root))
+
+(defun flymake-collection-mypy--default-directory (buffer)
+  "Find a directory from which to run mypy to check `BUFFER'.
+Try each method specified in `flymake-collection-mypy-project-root' in
+order and returns the first non-nil result."
+  (cl-dolist (spec flymake-collection-mypy-project-root)
+    (when-let
+        ((res (cond
+               ((eq spec 'default-directory)
+                (buffer-local-value 'default-directory buffer))
+               ((eq spec 'project-root)
+                (when-let ((proj (project-current)))
+                  (project-root proj)))
+               ((listp spec)
+                (flymake-collection-mypy--locate-dominating-files
+                 buffer spec))
+               ((stringp spec) spec))))
+      (cl-return res))))
+
 
 ;;;###autoload (autoload 'flymake-collection-mypy "flymake-collection-mypy")
 (flymake-collection-define-rx flymake-collection-mypy
@@ -38,22 +87,40 @@
 
 See URL `http://mypy-lang.org/'."
   :title "mypy"
-  :pre-let ((mypy-exec (executable-find "mypy")))
-  :pre-check (unless mypy-exec
-               (error "Cannot find mypy executable"))
+  :pre-let ((mypy-exec (executable-find "mypy"))
+            (default-directory (flymake-collection-mypy--default-directory
+                                flymake-collection-source)))
+  :pre-check (progn
+               (flymake-log :debug "Working dir is %s" default-directory)
+               (unless mypy-exec
+                 (error "Cannot find mypy executable"))
+               (unless default-directory
+                 (error "Default dir is nil: check `flymake-collection-mypy-project-root'")))
   :write-type 'file
   :source-inplace t
-  :command (list mypy-exec
-                 "--show-column-numbers"
-                 "--no-error-summary"
-                 "--no-color-output"
-                 "--show-absolute-path"
-                 "--show-error-codes"
-                 flymake-collection-temp-file)
+  :command `(,mypy-exec
+             "--show-column-numbers"
+             "--show-absolute-path"
+             "--no-error-summary"
+             "--no-color-output"
+             ,@flymake-collection-mypy-args
+             ,@(if-let ((source-file (buffer-file-name
+                                      flymake-collection-source))
+                        ((file-exists-p source-file)))
+                   (list
+                    "--shadow-file" source-file flymake-collection-temp-file
+                    source-file)
+                 (list flymake-collection-temp-file)))
+  ;; Temporary file cannot start with a dot for mypy, see
+  ;; https://github.com/mohkale/flymake-collection/pull/9
+  :temp-file-prefix "flymake_mypy_"
   :regexps
-  ((error   bol (file-name) ":" line ":" column ": error: "   (message) eol)
-   (warning bol (file-name) ":" line ":" column ": warning: " (message) eol)
-   (note    bol (file-name) ":" line ":" column ": note: "    (message) eol)))
+  ((error   bol (file-name) ":" line ":" column ": error: "
+            (message) (opt "  [" (id (* graph)) "]") eol)
+   (warning bol (file-name) ":" line ":" column ": warning: "
+            (message) (opt "  [" (id (* graph)) "]") eol)
+   (note    bol (file-name) ":" line ":" column ": note: "
+            (message) (opt "  [" (id (* graph)) "]") eol)))
 
 (provide 'flymake-collection-mypy)
 
